7cd58b07e0d98f8ffd6cd6ab297d91d95f065629
Gregory Lee <grlee77@gmail.com>
2020-03-11 03:29:58 -0400

    update _canny tests

--- cupyimg/skimage/feature/tests/test_canny.py
+++ cupyimg/skimage/feature/tests/test_canny.py
@@ -143,6 +143,12 @@ class TestCanny(unittest.TestCase):
             high_threshold=-100,
         )
 
+        # Example from issue #4282
+        image = data.camera()
+        self.assertRaises(ValueError, F.canny, image, use_quantiles=True,
+                          low_threshold=50, high_threshold=150)
+
+
     def test_dtype(self):
         """Check that the same output is produced regardless of image dtype."""
         image_uint8 = cp.asarray(data.camera())
